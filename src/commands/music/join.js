aebad8f99dea6e116f27f86b7df1e83e2f0dfcc4
Otaku Overlord <ghazanfar.ateeb9125@gmail.com>
2025-12-12 21:33:21 +0500

    Add "Speak" permission for client in Join command

--- src/commands/music/join.js
+++ src/commands/music/join.js
@@ -21,7 +21,7 @@ export default class Join extends Command {
       },
       permissions: {
         dev: false,
-        client: ["SendMessages", "ViewChannel", "EmbedLinks"],
+        client: ["SendMessages", "ViewChannel", "EmbedLinks", "Speak"],
         user: [],
       },
       slashCommand: true,
