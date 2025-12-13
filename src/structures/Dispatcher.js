21d0902432d510c8e35659280d16bad85a688225
Otaku Overlord <ghazanfar.ateeb9125@gmail.com>
2025-12-14 02:06:46 +0500

    debug: add comprehensive playback logging

--- src/structures/Dispatcher.js
+++ src/structures/Dispatcher.js
@@ -67,8 +67,24 @@ export default class Dispatcher {
         try {
           console.log('[Dispatcher] Attempting to play track:', this.current.info.title);
           console.log('[Dispatcher] Track encoded:', this.current.track ? 'Yes' : 'No');
-          await this.player.playTrack({ track: this.current.track });
+          console.log('[Dispatcher] Track length:', this.current.track?.length || 'N/A');
+          console.log('[Dispatcher] Player state before play:', {
+              playing: this.player.playing,
+              paused: this.player.paused,
+              track: this.player.track
+          });
+          
+          const result = await this.player.playTrack({ track: this.current.track });
+          console.log('[Dispatcher] playTrack result:', result);
+          
           await this.player.setGlobalVolume(80);
+          console.log('[Dispatcher] Volume set to 80');
+          
+          console.log('[Dispatcher] Player state after play:', {
+              playing: this.player.playing,
+              paused: this.player.paused,
+              track: this.player.track
+          });
         } catch (error) {
             logger.error('Failed to play track', error);
             this.queue.unshift(this.current);
@@ -77,6 +93,9 @@ export default class Dispatcher {
     }
 
     onStart() {
+        console.log('[Dispatcher] onStart event fired for:', this.current.info.title);
+        console.log('[Dispatcher] Player is now playing:', this.player.playing);
+        
         const embed = this.client.embed()
             .setTitle('Now Playing')
             .setDescription(`[${this.current.info.title}](${this.current.info.uri})`)
