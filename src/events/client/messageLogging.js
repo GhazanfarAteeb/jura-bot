469f47113c3f877d33499cf603e87fd1dd95f85c
Otaku Overlord <ghazanfar.ateeb9125@gmail.com>
2026-01-09 19:30:16 +0500

    refactor(messageLogging): remove caching of edited messages for editsnipe command

--- src/events/client/messageLogging.js
+++ src/events/client/messageLogging.js
@@ -1,7 +1,6 @@
 import { Events, EmbedBuilder } from 'discord.js';
 import Guild from '../../models/Guild.js';
 import { GLYPHS } from '../../utils/embeds.js';
-import { cacheEditedMessage } from '../../commands/utility/editsnipe.js';
 
 export default {
   name: 'messageLogging',
@@ -18,9 +17,6 @@ export default {
     client.on(Events.MessageUpdate, async (oldMessage, newMessage) => {
       if (!newMessage.guild || newMessage.author?.bot) return;
       if (oldMessage.content === newMessage.content) return; // Embed updates, etc.
-      
-      // Cache for editsnipe command
-      cacheEditedMessage(oldMessage, newMessage);
       
       await logMessageEdit(oldMessage, newMessage);
     });
